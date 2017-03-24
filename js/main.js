a3893032e87cdafb014fc68776a0c67dff35b2df
SanTech <github@serverintl.net>
2017-03-24 08:37:48 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function hideElement(){
+  document.getElementById("element").style.display="none";
+}
+function showElement(){
+  document.getElementById("element").style.display="block";
+}
---
+++ js/sch.js
@@ -0,0 +1,40 @@
+function showMonday(){
+  var x = document.getElementById("mon");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+function showTuesday(){
+  var x = document.getElementById("tue");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+function showWednesday(){
+  var x = document.getElementById("wed");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+function showThursday(){
+  var x = document.getElementById("thur");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+function showFriday(){
+  var x = document.getElementById("fri");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
