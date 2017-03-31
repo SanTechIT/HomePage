1e7b85dab50834259f7bf4582710c332252a92e7
SanTech <github@serverintl.net>
2017-03-30 23:03:19 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ function hideElement(){
 function showElement(){
   document.getElementById("element").style.display="block";
 }
+function isMobileDevice() {
+  testExp = new RegExp('Android|webOS|iPhone|iPad|' +
+    		       'BlackBerry|Windows Phone|'  +
+    		       'Opera Mini|IEMobile|Mobile' ,
+    		      'i');
+  if (testExp.test(navigator.userAgent))
+       document.getElementById("test").innerHTML =
+       		"Your device is a Mobile Device and this site is not made for mobile devices, therefore you may experince some  anomalies";
+  else
+       document.getElementById("test").innerHTML =
+       		"Your device is NOT a Mobile Device. Go ahead to the site!";
+//Original Script from http://www.opentechguides.com/how-to/article/javascript/98/detect-mobile-device.html
+}
